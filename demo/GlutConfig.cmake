517067666527a187849bee437b0c8bb3e52143f2
ilya glushchenko <ilia.glushchenko@gmail.com>
2017-05-03 00:07:36 +0300

    GLUT submodule config is moved in a separate file.

---
+++ demo/GlutConfig.cmake
@@ -0,0 +1,21 @@
+cmake_minimum_required (VERSION 3.1)
+
+#Configure GLUT
+set(FREEGLUT_BUILD_DEMOS OFF CACHE BOOL "Build FreeGLUT demos.")
+set(FREEGLUT_BUILD_SHARED_LIBS OFF CACHE BOOL "Build FreeGLUT shared library.")
+set(FREEGLUT_PRINT_ERRORS OFF CACHE BOOL "Lib prints errors to stderr")
+set(FREEGLUT_PRINT_WARNINGS OFF CACHE BOOL "Lib prints warnings to stderr")
+
+#Set GLUT variables
+set(GLUT_PATH
+    ${CMAKE_CURRENT_SOURCE_DIR}/../third_party/FreeGLUT/freeglut/freeglut
+)
+set(GLUT_LIBRARY_DIRS
+    ${CMAKE_CURRENT_BINARY_DIR}/third_party/FreeGLUT/freeglut/freeglut
+)
+set(GLUT_INCLUDE_DIRS
+    ${CMAKE_CURRENT_SOURCE_DIR}/../third_party/FreeGLUT/freeglut/freeglut/include
+)
+
+add_subdirectory(${GLUT_PATH} ${GLUT_LIBRARY_DIRS})
+set(GLUT_LIBRARIES freeglut_static)
