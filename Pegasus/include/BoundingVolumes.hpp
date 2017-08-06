1406e5fcbcfa29cbfa8099fbeb4043e655da5461
Alexandr Yanenko <yanenkoalexandr@gmail.com>
2017-08-06 21:11:04 +0300

    [#45]Finished bounding volume hierarchy

--- Pegasus/include/BoundingVolumes.hpp
+++ Pegasus/include/BoundingVolumes.hpp
@@ -11,6 +11,11 @@
 #include <vector>
 #include <set>
 #include <array>
+#include <algorithm>
+#include <random>
+#include <unordered_map>
+#include <iostream>
+#include <glm/ext.hpp>
 
 namespace pegasus
 {
@@ -133,6 +138,204 @@ private:
     );
 };
 } // namespace sphere
+
+namespace hierarchy
+{
+
+
+
+template <typename BoundingVolume>
+class BoundingVolumeHierarchy
+{
+public:
+    BoundingVolumeHierarchy(Shape const& shape, Indices const& indices)
+        : m_shape(shape)
+        , m_indices(indices)
+        , m_volume(BoundingVolume(shape, indices))
+    {
+        if (indices.size() <= MAX_NODE_SIZE)
+        {
+            return;
+        }
+
+        Vertices shapeVertices = GetShapeVertices();
+        glm::dvec3 maxDistanceVector = GetMaximumDistanceDirectionApprox(shapeVertices);
+        glm::dvec3 min = GetMinVectorAlongDirection(maxDistanceVector, shapeVertices);
+        math::Plane minPlane(min, maxDistanceVector);
+
+        CentroidsMap centroids = GetFacesCentroids();
+        Indices lowerIndices, upperIndices;
+        std::tie(lowerIndices, upperIndices) = GetPartitionIndices(minPlane, centroids);
+
+        std::cout << lowerIndices.size() << " " << upperIndices.size() << "\n";
+
+        lowerChild = std::make_unique<BoundingVolumeHierarchy<BoundingVolume>>(m_shape, lowerIndices);
+        upperChild = std::make_unique<BoundingVolumeHierarchy<BoundingVolume>>(m_shape, upperIndices);
+    }
+
+    std::unique_ptr<BoundingVolumeHierarchy> const& getLowerChild() const
+    {
+        return lowerChild;
+    }
+
+    std::unique_ptr<BoundingVolumeHierarchy> const& getUpperChild() const
+    {
+        return upperChild;
+    }
+
+    BoundingVolume const getVolume() const
+    {
+        return m_volume;
+    }
+
+private:
+    using CentroidsMap = std::unordered_map<std::size_t, glm::dvec3>;
+
+    std::size_t static const MAX_NODE_SIZE = 5;
+
+    Shape const& m_shape;
+    Indices const& m_indices;
+    BoundingVolume const m_volume;
+
+    std::unique_ptr<BoundingVolumeHierarchy<BoundingVolume>> lowerChild;
+    std::unique_ptr<BoundingVolumeHierarchy<BoundingVolume>> upperChild;
+
+    Vertices GetShapeVertices()
+    {
+        Vertices result;
+        result.reserve(3 * m_indices.size());
+        for (std::size_t i : m_indices)
+        {
+            result.insert(result.end(), {
+                m_shape.vertices[m_shape.faces[i][0]],
+                m_shape.vertices[m_shape.faces[i][1]],
+                m_shape.vertices[m_shape.faces[i][2]]
+            });
+        }
+        return result;
+    }
+
+    glm::dvec3 GetMaximumDistanceDirectionApprox(Vertices const& vertices)
+    {
+        glm::dvec3
+            xMin(vertices[0]), yMin(vertices[0]), zMin(vertices[0]),
+            xMax(vertices[0]), yMax(vertices[0]), zMax(vertices[0]);
+
+        for (glm::dvec3 const& vertice : vertices)
+        {
+            if (vertice.x < xMin.x) xMin = vertice;
+            if (vertice.y < yMin.y) yMin = vertice;
+            if (vertice.z < zMin.z) zMin = vertice;
+            if (vertice.x > xMax.x) xMax = vertice;
+            if (vertice.y > yMax.y) yMax = vertice;
+            if (vertice.z > zMax.z) zMax = vertice;
+        }
+
+        glm::dvec3 min = xMin, max = xMax;
+
+        double dist2x = glm::distance2(xMin, xMax);
+        double dist2y = glm::distance2(yMin, yMax);
+        double dist2z = glm::distance2(zMin, zMax);
+
+        if (dist2y > dist2x && dist2y > dist2z)
+        {
+            min = yMin;
+            max = yMax;
+        }
+        if (dist2z > dist2y && dist2z > dist2x)
+        {
+            min = zMin;
+            max = zMax;
+        };
+
+        return glm::normalize(max - min);
+    }
+
+    glm::dvec3 GetMinVectorAlongDirection(glm::dvec3 maxDistanceVector, Vertices vertices)
+    {
+        return *std::min_element(
+            vertices.begin(),
+            vertices.end(),
+            [&maxDistanceVector](glm::dvec3 const& a, glm::dvec3 const& b) -> bool
+            {
+                return glm::dot(a, maxDistanceVector) < glm::dot(b, maxDistanceVector);
+            });
+    }
+
+    CentroidsMap GetFacesCentroids()
+    {
+        CentroidsMap centroids;
+        for (std::size_t index : m_indices)
+        {
+            Face const& currFace = m_shape.faces[index];
+            centroids[index] = pegasus::math::CalculateCentroid(
+                {
+                    m_shape.vertices[currFace[0]],
+                    m_shape.vertices[currFace[1]],
+                    m_shape.vertices[currFace[2]]
+                }
+            );
+        }
+        return centroids;
+    }
+
+    std::pair<Indices, Indices> GetPartitionIndices(math::Plane const& minPlane, CentroidsMap const& centroids)
+    {
+        std::vector<std::size_t> indicesArr;
+        std::unordered_map<std::size_t, double> distances;
+        indicesArr.reserve(centroids.size());
+        for (auto const& keyValuePair : centroids)
+        {
+            std::size_t index;
+            glm::dvec3 centroid;
+            std::tie(index, centroid) = keyValuePair;
+
+            indicesArr.push_back(index);
+            distances[index] = minPlane.DistanceToPoint(centroid);
+        }
+
+        std::size_t middleIndex = indicesArr.size() / 2;
+        std::size_t left = 0, right = indicesArr.size();
+        std::random_device(rd);
+        std::mt19937 rng(rd());
+        while (true)
+        {
+            std::uniform_int_distribution<std::size_t> distr(left, right - 1);
+            std::size_t pivot = distr(rng);
+            std::size_t pivotValue = indicesArr[pivot];
+
+            std::iter_swap(indicesArr.begin() + pivot, indicesArr.begin() + right - 1);
+            std::size_t pivotIndex = left;
+            for (std::size_t i = left; i < right - 1; ++i)
+            {
+                if (distances[indicesArr[i]] < distances[pivotValue])
+                {
+                    std::iter_swap(indicesArr.begin() + i, indicesArr.begin() + pivotIndex);
+                    ++pivotIndex;
+                }
+            }
+            std::iter_swap(indicesArr.begin() + right - 1, indicesArr.begin() + pivotIndex);
+
+            if (middleIndex == pivotIndex)
+            {
+                break;
+            }
+            if (middleIndex < pivotIndex)
+            {
+                right = pivotIndex;
+            }
+            else
+            {
+                left = pivotIndex + 1;
+            }
+        }
+
+        Indices lowerIndices(indicesArr.begin(), indicesArr.begin() + middleIndex);
+        Indices upperIndices(indicesArr.begin() + middleIndex, indicesArr.end());
+        return std::make_pair(std::move(lowerIndices), std::move(upperIndices));
+    }
+};
+} // namespace hierarchy
 } // namespace volumes
 } // namespace geometry
 } // namespace pegasus
