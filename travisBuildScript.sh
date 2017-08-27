4d80c18f28516a122dc5ba2683125a9447155111
Dmitry Odintsov <odintsoff@gmail.com>
2017-08-27 12:43:38 +0300

    CLang build configuration

--- travisBuildScript.sh
+++ travisBuildScript.sh
@@ -6,7 +6,11 @@ cd $BUILD_DIR
 
 CMAKE_FLAGS="-DCMAKE_CXX_COMPILER=$COMPILER"
 
+if [ -n "$COMPILER_FLAGS" ]; then
+    CMAKE_FLAGS="$CMAKE_FLAGS -DCMAKE_CXX_FLAGS=$COMPILER_FLAGS"
+fi
+
 cmake $CMAKE_FLAGS ..
 
-make -j${nproc} $COMPILER_FLAGS
+make -j${nproc}
 ctest -VV
