f5738f487b6b41769ceeeadf23bbe521101866d6
Ilya Glushchenko <ilia.glushchenko@gmail.com>
2018-02-18 16:57:31 +0300

    [#102] IMGUI submodule

---
+++ cmake/ImguiConfig.cmake
@@ -0,0 +1,12 @@
+# Copyright (C) 2017 by Godlike
+# This code is licensed under the MIT license (MIT)
+# (http://opensource.org/licenses/MIT)
+
+set(IMGUI_INCLUDE_DIR "${PEGASUS_ROOT}/third_party/imgui")
+set(IMGUI_SOURCE_DIR "${PEGASUS_ROOT}/third_party/imgui")
+set(IMGUI_BINARY_DIR "${CMAKE_BINARY_DIR}/imgui" CACHE STRING "Path to IMGUI binary directory")
+set(IMGUI_SOURCES
+	${IMGUI_INCLUDE_DIR}/imgui.cpp
+	${IMGUI_INCLUDE_DIR}/imgui_draw.cpp
+	${IMGUI_INCLUDE_DIR}/imgui_demo.cpp
+)
