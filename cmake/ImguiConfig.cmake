128bd03476c618eea1b0d4656bc94074a59414d0
Dmitry Odintsov <odintsoff@gmail.com>
2018-07-12 19:16:57 -0400

    [#118] Rename demo/imgui project to demo/dependencies

--- cmake/ImguiConfig.cmake
+++ cmake/ImguiConfig.cmake
@@ -2,7 +2,7 @@
 # This code is licensed under the MIT license (MIT)
 # (http://opensource.org/licenses/MIT)
 
-set(IMGUI_INCLUDE_DIR "${PEGASUS_ROOT}/third_party/imgui" "${PEGASUS_ROOT}/demo/imgui")
-set(IMGUI_SOURCE_DIR "${PEGASUS_ROOT}/third_party/imgui" "${PEGASUS_ROOT}/demo/imgui")
+set(IMGUI_INCLUDE_DIR "${PEGASUS_ROOT}/third_party/imgui" "${PEGASUS_ROOT}/demo/dependencies/imgui")
+set(IMGUI_SOURCE_DIR "${PEGASUS_ROOT}/third_party/imgui" "${PEGASUS_ROOT}/demo/dependencies/imgui")
 set(IMGUI_BINARY_DIR "${CMAKE_BINARY_DIR}/imgui" CACHE STRING "Path to IMGUI binary directory")
 
